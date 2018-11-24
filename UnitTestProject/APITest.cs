d0fdb43c196fa67115a800c84f421f63ccb2f5df
Greicy de Mattia Silva <greicy_matias@hotmail.com>
2018-11-23 22:19:43 -0200

    Commit Inicial
    Adicionando o projeto no repositório do Github.

---
+++ UnitTestProject/APITest.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebApplication.Controllers;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class APITest
+    {
+        [TestMethod]
+        public void Deveria_retornar_url_do_github()
+        {
+            //Arrange           
+            ValuesController controller = new ValuesController();
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            result.Value.Should().StartWith("https://github.com/");
+        }
+
+        [DataTestMethod]
+        [DataRow(100, 5, "105,10")]
+        [DataRow(100, 3, "103,03")]
+        [DataRow(0, 3, "0,00")]
+        public void Deveria_calcular_juros(int initialValue, int months, string expectedValue)
+        {
+            //Arrange
+            ValuesController controller = new ValuesController();
+
+            //Act
+            var result = controller.Get(initialValue, months);
+
+            //Assert
+            result.Value.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("showmethecode")]
+        public ActionResult<string> Get()
+        {
+            return "https://github.com/GreicyMatias/Asp.NetCoreSample";
+        }
+
+        [HttpGet("calculajuros")]
+        public ActionResult<string> Get([FromQuery(Name = "valorinicial")] int initialValue, [FromQuery(Name = "meses")] int months)
+        {
+            return (initialValue * Math.Pow((1 + 0.01), months)).ToString("N2");
+        }
+    }
+}
